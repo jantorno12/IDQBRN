47e925dae404bf17479a14343975dc94305fb1b0
jantorno12 <danieljantorno@hotmail.com>
2022-06-05 18:58:24 -0300

    user atualizado

--- labprog3/src/views/User.js
+++ labprog3/src/views/User.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 // reactstrap components
 import {
@@ -15,7 +15,46 @@ import {
   Col,
 } from "reactstrap";
 
-function User() {
+const axios = require('axios').default;
+
+function User({history}) {
+  const [name, setName] = useState('');
+  const [prev, setPrev] = useState('');
+  const [area, setArea] = useState('');
+  const [agnt, setAgnt] = useState('');
+  const [cont, setCont] = useState('');
+  const [mprev, setMprev] = useState('');
+  const [trans, setTrans] = useState('');
+  const [apclin, setApclin] = useState('');
+  const [unref, setUnref] = useState('');
+
+  const cadastro = {name, prev, area, agnt, cont, mprev, trans, apclin, unref}
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({nome:data.get("name")})
+    console.log(cadastro)
+    //Envia para o backend
+    axios.post('http://localhost:5000/admin/user-page',cadastro)
+    .then((response)=>{
+      history.push('/dashboard')
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
+  // Envia para o backend
+  function enter(){
+    axios.post('http://localhost:5000/admin/user-page', {
+      usuario: name
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   return (
     <>
       <div className="content">
@@ -26,101 +65,121 @@ function User() {
                 <CardTitle tag="h5">Cadastrar Doença</CardTitle>
               </CardHeader>
               <CardBody>
-                <Form>
-                  <Row>
-                    <Col className="pr-1">
-                      <FormGroup>
-                        <label>Nome da Doença</label>
-                        <Input
-                          placeholder="Nome"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-1">
-                      <FormGroup>
-                        <label> Prevalência </label>
-                        <Input placeholder="Prevalência" type="text" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-1" md="6">
-                      <FormGroup>
-                        <label> Área de Risco de Contágio</label>
-                        <Input
-                          placeholder="Área de risco"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-1" md="6">
-                      <FormGroup>
-                        <label>Agente Infeccioso</label>
-                        <Input
-                          placeholder="Agente infeccioso"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="12">
-                      <FormGroup>
-                        <label>Formas de Contágio</label>
-                        <Input
-                          placeholder="Formas de Contágio"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col>
-                      <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Medidas de Prevenção</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <form onSubmit={handleSubmit}>
+                  {/* <Form> */}
+                    <Row>
+                      <Col className="pr-1">
+                        <FormGroup>
+                          <label>Nome da Doença</label>
+                          <Input
+                            placeholder="Nome"
+                            type="text"
+                            value={name}
+                            id = "name"
+                            onChange={(e)=>{setName(e.target.value);console.log(cadastro)}}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1">
+                        <FormGroup>
+                          <label> Prevalência </label>
+                          <Input 
+                          placeholder="Prevalência" 
+                          type="text" 
+                          value = {prev}
+                          onChange={(e)=>setPrev(e.target.value)}
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1" md="6">
+                        <FormGroup>
+                          <label> Área de Risco de Contágio</label>
+                          <Input
+                            placeholder="Área de risco"
+                            type="text"
+                            value = {area}
+                            onChange={(e)=>setArea(e.target.value)}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="6">
+                        <FormGroup>
+                          <label>Agente Infeccioso</label>
+                          <Input
+                            placeholder="Agente infeccioso"
+                            type="text"
+                            value = {agnt}
+                            onChange={(e)=>setAgnt(e.target.value)}
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="12">
+                        <FormGroup>
+                          <label>Formas de Contágio</label>
+                          <Input
+                            placeholder="Formas de Contágio"
+                            type="text"
+                            value = {cont}
+                            onChange={(e)=>setCont(e.target.value)}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col>
+                        <div className="form-group">
+                          <label htmlFor="exampleFormControlTextarea1">Medidas de Prevenção</label>
+                          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={mprev} onChange={(e)=>setMprev(e.target.value)}></textarea>
+                        </div>
+                      </Col>
+                      <Col>
+                        <div className="form-group">
+                          <label htmlFor="exampleFormControlTextarea1">Transmissibilidade</label>
+                          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={trans} onChange={(e)=>setTrans(e.target.value)}></textarea>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1">
+                        <FormGroup>
+                          <label>Apresentação Clínica</label>
+                          <Input
+                            placeholder="Apresentação Clínica"
+                            type="multiline"
+                            value = {apclin}
+                            onChange={(e)=>setApclin(e.target.value)}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-1">
+                        <FormGroup>
+                          <label>Unidades de Saúde de Referência</label>
+                          <Input
+                            placeholder="Unidades Referência"
+                            type="multiline"
+                            value = {unref}
+                            onChange={(e)=>setUnref(e.target.value)}
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                    </Row>
+                    <Row>
+                      <div className="update ml-auto mr-auto">
+                        <Button
+                          className="btn-round"
+                          color="primary"
+                          type="submit"
+                        >
+                          Cadastrar
+                        </Button>
                       </div>
-                    </Col>
-                    <Col>
-                      <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Transmissibilidade</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-1">
-                      <FormGroup>
-                        <label>Apresentação Clínica</label>
-                        <Input
-                          placeholder="Apresentação Clínica"
-                          type="multiline"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-1">
-                      <FormGroup>
-                        <label>Unidades de Saúde de Referência</label>
-                        <Input
-                          placeholder="Unidades Referência"
-                          type="multiline"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                  </Row>
-                  <Row>
-                    <div className="update ml-auto mr-auto">
-                      <Button
-                        className="btn-round"
-                        color="primary"
-                        type="submit"
-                      >
-                        Cadastrar
-                      </Button>
-                    </div>
-                  </Row>
-                </Form>
+                    </Row>
+                  {/* </Form> */}
+                </form>
               </CardBody>
             </Card>
           </Col>
